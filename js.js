24fc3feafcc22475ce541ffefda839360a654d63
Abdulkafi88 <123754218+Abdulkafi88@users.noreply.github.com>
2023-01-30 07:33:53 -0800

    Update js.js

--- js.js
+++ js.js
@@ -1,3 +1,21 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+
+hamburger.addEventListener("click", mobileMenu);
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+
 function todo(){
     let newp=document.createElement('li')
     let inputvuale=document.getElementById('input').value
@@ -26,4 +44,53 @@ function todo(){
     })
 }
 
+
+document.getElementById('red').addEventListener('click',abdul)
+
+function abdul(){
+    document.querySelector('body').style.background='red'
+}
+
+
+let any=document.getElementById('bule').addEventListener('click', nirig)
+
+function nirig(){
+    document.querySelector('body').style.background='blue'
+}
+
+let more=document.getElementById('orange').addEventListener('click', yo)
+
+function yo(){
+    document.querySelector('body').style.background='orange'
+}
+
+let total= 0
+
+document.querySelector('#pumpkin').addEventListener('click', yy)
+
+document.querySelector('#dominosPizza').addEventListener('click', mm)
+document.querySelector('#Zebra').addEventListener('click', kk)
+document.querySelector('#luffy4').addEventListener('click', nn)
+
+function yy(){
+    total=0
+    document.querySelector('#placeToputResult').innerText=total
+    
+}
+
+function mm(){
+    total=total+3  
+    document.querySelector('#placeToputResult').innerText=total
+}
+
+function kk(){
+    total= total + 9
+    document.querySelector('#placeToputResult').innerText=total
+}
+
+
+function nn(){
+    total=total-2
+    document.getElementById('placeToputResult').innerText=total
+}
 
