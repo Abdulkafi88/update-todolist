1a774bba0633c7aa5788c520bae55be8436fb751
Abdulkafi88 <123754218+Abdulkafi88@users.noreply.github.com>
2023-03-25 12:51:31 -0700

    Update js.js

--- js.js
+++ js.js
@@ -1,120 +1,72 @@
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".nav-menu");
-// const navLink = document.querySelectorAll(".nav-link");
-
-// hamburger.addEventListener("click", mobileMenu);
-// navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-// function mobileMenu() {
-//     hamburger.classList.toggle("active");
-//     navMenu.classList.toggle("active");
-// }
-
-// function closeMenu() {
-//     hamburger.classList.remove("active");
-//     navMenu.classList.remove("active");
-// }
+const todoInput=document.querySelector('.todo-input');
+const todoButton=document.querySelector('.todo-button');
+const todoList=document.querySelector('.todo-list');
 
 
-function todo(){
-    let newp=document.createElement('li')
-    let inputvuale=document.getElementById('input').value
-    let textNode=document.createTextNode(inputvuale)
-    newp.appendChild(textNode)
-
-    if(inputvuale===''){
-        alert('add new task')
-    }
-
-    else{
-        document.getElementById('list').appendChild(newp)
+todoButton.addEventListener('click',addTodo);
 
 
-    }
+todoList.addEventListener('click', deleteCheck)
 
-    document.getElementById('input').value=''
 
-    newp.addEventListener('click', function(){
-        newp.style.textDecoration='line-through'
+function addTodo(e){
+    e.preventDefault()
+//making new div
+    let newDive=document.createElement('div');
+    newDive.classList.add('todo');
+    // //making the li
+    let newLi=document.createElement('li')
+    newLi.innerText=todoInput.value
+    todoInput.value=''
+    newLi.classList.add('newitem')
+    newDive.appendChild(newLi)
 
-    })
+    //making the complete btn
+    let comPletedButton=document.createElement('button');
+    comPletedButton.innerHTML='<i class="fa-solid fa-check"></i>'
+    comPletedButton.classList.add('check')
+    newDive.appendChild(comPletedButton)
 
-    newp.addEventListener('dblclick',  function(){
-        newp.removeChild(textNode)
-    })
+    //making the trash btn
+
+    let trash=document.createElement('trash-button');
+    trash.innerHTML='<i class="fa-solid fa-trash"></i>'
+    trash.classList.add('trash')
+    newDive.appendChild(trash)
+   
+
+    //append the newDiv to the big div
+
+    todoList.appendChild(newDive)
+
+    
 }
 
 
-
-// document.getElementById('red').addEventListener('click',abdul)
-
-// function abdul(){
-//     document.querySelector('body').style.background='red'
-// }
-
-
-// let any=document.getElementById('bule').addEventListener('click', nirig)
-
-// function nirig(){
-//     document.querySelector('body').style.background='blue'
-// }
-
-// let more=document.getElementById('orange').addEventListener('click', yo)
-
-// function yo(){
-//     document.querySelector('body').style.background='orange'
-// }
-
-// let total= 0
-
-// document.querySelector('#pumpkin').addEventListener('click', yy)
-
-// document.querySelector('#dominosPizza').addEventListener('click', mm)
-// document.querySelector('#Zebra').addEventListener('click', kk)
-// document.querySelector('#luffy4').addEventListener('click', nn)
-
-// function yy(){
-//     total=0
-//     document.querySelector('#placeToputResult').innerText=total
-    
-// }
-
-// function mm(){
-//     total=total+3  
-//     document.querySelector('#placeToputResult').innerText=total
-// }
-
-// function kk(){
-//     total= total + 9
-//     document.querySelector('#placeToputResult').innerText=total
-// }
+function deleteCheck(event) {
+    let item=event.target
+    if(item.classList[0]==='trash'){
+     const todo=item.parentElement;
+     todo.remove()
+     
+    }
+     console.log(event.target)
 
 
-// function nn(){
-//     total=total-2
-//     document.getElementById('placeToputResult').innerText=total
-// }
+     if(item.classList[0]==='check'){
+        const todo=item.parentElement;
+        todo.classList.toggle('check1')
+     }
 
 
 
 
 
-// const hamburger = document.querySelector(".hamburger");
-// const navMenu = document.querySelector(".nav-menu");
-// const navLink = document.querySelectorAll(".nav-link");
 
-// hamburger.addEventListener("click", mobileMenu);
-// navLink.forEach(n => n.addEventListener("click", closeMenu));
+  
+     
+ }
 
-// function mobileMenu() {
-//     hamburger.classList.toggle("active");
-//     navMenu.classList.toggle("active");
-// }
-
-// function closeMenu() {
-//     hamburger.classList.remove("active");
-//     navMenu.classList.remove("active");
-// }
 
 
 
